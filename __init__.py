ad0369cf19d3bcd8b616f353869d98d8350432da
c4rlosdias <c4rlos.dias@gmail.com>
2025-06-20 08:55:23 -0300

    first commit

---
+++ __init__.py
@@ -0,0 +1,90 @@
+./venv/scripts# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+bl_info = {
+    "name"        : "Oil&Gas Tools",
+    "author"      : "Carlos Dias",
+    "description" : "",
+    "blender"     : (4, 3, 0),
+    "version"     : (0, 0, 1),
+    "location"    : "View3D > Panel > Oil&Gas Tools",
+    "warning"     : "",
+    "category"    : "User"
+}
+
+
+import sys
+import os
+from bpy.props import PointerProperty
+from bpy.types import Scene
+from bpy.utils import register_class, unregister_class
+
+
+
+#if sys.modules.get("bpy", None):
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)), "libs", "site", "packages"))
+
+from .operators import *
+from .panels import *
+from .properties import *
+
+classes = [     
+    Operator_get_properties,
+    Operator_get_classes,
+    Operator_load_decomposition,
+    Operator_contract_classes,
+    Operator_expand_classes,
+    Operator_contract_decomposition,
+    Operator_expand_decomposition,
+    Operator_add_properties,
+    Operator_clear_properties,
+    Operator_get_prop_info,
+    Operator_get_class_info,
+    Operator_assign_all,
+    Operator_unassign_all,
+    Operator_uri,
+    Operator_create,
+    Operator_export_ids,
+    Operator_element_selection,
+    Operator_catalog_export,
+    Panel_Connect,
+    Panel_Import_Properties,
+    Panel_Import_Classes,
+    Panel_Export_Properties, 
+    Panel_Decompositions,  
+    Panel_Catalog, 
+    BIM_UL_ifc_properties,
+    BIM_UL_classes,
+    BIM_UL_property_class,
+    BIM_UL_decomposition,
+    Ifc_properties,
+    Class_info,
+    Container,
+    MyProperties,
+]
+
+def register():
+    for c in classes:
+        register_class(c)
+    Scene.my_props = PointerProperty(type=MyProperties)
+
+
+def unregister():
+    del Scene.my_props
+    for c in classes:
+        unregister_class(c)
+
+if __name__ == "__main__":
+    register()
+
---
+++ properties.py
@@ -0,0 +1,85 @@
+import bpy
+from bpy.types import PropertyGroup
+from bpy.props import *
+import bonsai.tool as tool
+import requests
+from .data import bSDD
+
+def get_dictionaries(self, context):                
+    if not bSDD.is_loaded:
+        bSDD.load_dictionaries()
+    return bSDD.data_dic
+        
+def active_prop_changed(self, context):
+    self.info_prop_loaded = False
+    self.class_info.clear()
+
+def active_class_changed(self, context):
+    #self.classes_shown.clear()
+    self.classes_loaded = False
+    
+
+class Ifc_properties(PropertyGroup):       
+    name        : StringProperty(name='name')
+    code        : StringProperty(name='code')
+    description : StringProperty(name='description')
+    uri         : StringProperty(name="uri")
+    is_selected : BoolProperty(name="is selected", default=True)
+
+class Class_info(PropertyGroup):
+    code        : StringProperty(name='code')
+    name        : StringProperty(name='name')
+    description : StringProperty(name='description')
+    uri         : StringProperty(name='uri')    
+    propertyset : StringProperty(name='property set')
+    has_children: BoolProperty(name="has children")    
+    is_hidden   : BoolProperty(name="is Hidded", default=True)
+    is_expanded : BoolProperty(name="Is Expanded", default=True)
+    index       : IntProperty(name="index")
+    parent      : StringProperty(name="parent")
+    level_index : IntProperty(name="level index")
+    type        : StringProperty(name="class type")
+
+class Container(PropertyGroup):
+    has_children: BoolProperty(name="has children")    
+    is_hidden   : BoolProperty(name="is Hidded", default=True)
+    is_expanded : BoolProperty(name="Is Expanded", default=True)
+    is_selected : BoolProperty(name="Is Selected")
+    index       : IntProperty(name="index")
+    id          : IntProperty(name="id")
+    parent      : StringProperty(name="parent")
+    level       : IntProperty(name="level")
+    type        : StringProperty(name="element type")   
+    name        : StringProperty(name="name") 
+
+
+class MyProperties(PropertyGroup): 
+    
+    active_info_prop_index   : IntProperty(name='object index', default=0)
+    active_property_index    : IntProperty(name='property index', default=0, update=active_prop_changed)
+    active_class_index       : IntProperty(name='class index', default=0, update=active_class_changed)
+    active_element_index     : IntProperty(name='element index', default=0)
+    add_prop_clicked         : BoolProperty(name="add property clicked", default=False)
+    class_info               : CollectionProperty(name='info class', type=Class_info)
+    classes                  : CollectionProperty(name='classes', type=Class_info) 
+    classes_shown            : CollectionProperty(name='classes', type=Class_info) 
+    elements_containers      : CollectionProperty(name='elements containers', type=Container) 
+    containers_show          : CollectionProperty(name='containers show', type=Container) 
+    class_description        : StringProperty(name='class description')
+    class_definition         : StringProperty(name='class definition')
+    class_version            : StringProperty(name='class version date') 
+    class_type               : StringProperty(name='class type') 
+    class_ifctype            : StringProperty(name='ifc class') 
+    classes_loaded           : BoolProperty(name='classes loaded', default=False)   
+    dictionaries_loaded      : BoolProperty(name='Dictionaries loaded')
+    dictionary               : EnumProperty(items=get_dictionaries, name='',  description='Get Dictionaries')    
+    prop_datatype            : StringProperty(name='property data type')
+    prop_units               : StringProperty(name='property units')
+    prop_type                : StringProperty(name='property type')
+    prop_description         : StringProperty(name='property description')
+    prop_definition          : StringProperty(name='property definition')
+    ifc_prop                 : CollectionProperty(name='properties', type=Ifc_properties) 
+    info_prop_loaded         : BoolProperty(name='info prop loaded', default=False)
+    info_class_loaded        : BoolProperty(name='info class loaded', default=False)
+    ids_file                 : StringProperty(name='IDS file')
+
